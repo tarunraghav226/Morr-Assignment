177dbc9eaf3b5e2854e0c8e4b7cdb31631e9eacb
tarunraghav226 <tarun.raghav226@gmail.com>
2021-01-23 13:30:03 +0530

    Added APIView to get searched result.

--- morr/contacts/views.py
+++ morr/contacts/views.py
@@ -29,3 +29,17 @@ class UpdateContactAPIView(generics.UpdateAPIView):
     queryset = ContactsModel.objects.all()
     serializer_class = ContactSerializer
     lookup_field = "_id"
+
+
+class SearchContactAPIVIew(generics.ListAPIView):
+    serializer_class = ContactSerializer
+
+    def get_queryset(self):
+        if self.request.GET.get("name",None) and self.request.GET.get("email_id",None):
+            return ContactsModel.objects.filter(name = self.request.GET["name"], email_id = self.request.GET["email_id"])
+        elif self.request.GET.get("name",None):
+            return ContactsModel.objects.filter(name = self.request.GET["name"])
+        elif self.request.GET.get("email_id",None):
+            return ContactsModel.objects.filter(email_id = self.request.GET["email_id"])
+        else:
+            return ContactsModel.objects.all()
