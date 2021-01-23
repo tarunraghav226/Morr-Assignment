0458cdb2702a23c9c637f4ab44d2ca2635728018
tarunraghav226 <tarun.raghav226@gmail.com>
2021-01-23 14:07:03 +0530

    Added documentation to each view.

--- morr/contacts/views.py
+++ morr/contacts/views.py
@@ -5,34 +5,53 @@ from .serializers import ContactSerializer
 
 # Create your views here.
 class ContactListAPIView(generics.ListAPIView):
+    """
+        This view returns the list of contacts available.
+    """
     queryset = ContactsModel.objects.all()
     serializer_class = ContactSerializer
     pagination_class = None
 
 
 class ContactDetailAPIView(generics.RetrieveAPIView):
+    """
+        This view returns the detail of specific contact.
+    """
     queryset = ContactsModel.objects.all()
     serializer_class = ContactSerializer
     lookup_field = "_id"
 
 
 class AddContactAPIView(generics.CreateAPIView):
+    """
+        This view adds a new contact in database.
+    """
     serializer_class = ContactSerializer
 
 
 class DeleteContactAPIView(generics.DestroyAPIView):
+    """
+        This view deletes the contact from database.
+    """
     queryset = ContactsModel.objects.all()
     serializer_class = ContactSerializer
     lookup_field = "_id"
 
 
 class UpdateContactAPIView(generics.UpdateAPIView):
+    """
+        This view updates the contact if available.
+    """
     queryset = ContactsModel.objects.all()
     serializer_class = ContactSerializer
     lookup_field = "_id"
 
 
 class SearchContactAPIVIew(generics.ListAPIView):
+    """
+        This view returns the list of contacts available after searching them in database and 
+        also uses pagination.
+    """
     serializer_class = ContactSerializer
 
     def get_queryset(self):
