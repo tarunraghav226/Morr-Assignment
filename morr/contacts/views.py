2c37b4abe5bbca0414b663fbf37bee86bda3600e
tarunraghav226 <tarun.raghav226@gmail.com>
2021-01-23 11:48:17 +0530

    Added ContactListAPIView.

--- morr/contacts/views.py
+++ morr/contacts/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import ContactsModel
+from .serializers import ContactSerializer
 
 # Create your views here.
+class ContactListAPIView(generics.ListAPIView):
+    queryset = ContactsModel.objects.all()
+    serializer_class = ContactSerializer
