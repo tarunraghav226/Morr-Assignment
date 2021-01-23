c333d49c755a30ce07d0f01301abf592ceaf81f5
tarunraghav226 <tarun.raghav226@gmail.com>
2021-01-23 11:41:09 +0530

    Added contacts model.

--- morr/contacts/models.py
+++ morr/contacts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class ContactsModel(models.Model):
+    _id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=20)
+    email_id = models.CharField(unique=True, max_length=30)
+
+    def __str__(self):
+        return self._id + " " + self.name + " " + self.email_id
