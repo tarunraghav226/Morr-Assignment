db52956c2917d6b786b8a64eb3a8f38cfb002d92
tarunraghav226 <tarun.raghav226@gmail.com>
2021-01-23 11:56:43 +0530

    Added GET api endpoint.

--- morr/morr/urls.py
+++ morr/morr/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from contacts import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api_get/', views.ContactListAPIView.as_view())
 ]
